6ce3ca69d893e5592fafb489ee934d7671d74272
Eric Elliott <support@paralleldrive.com>
2015-03-01 20:42:54 -0700

    Add test for .where() multi-clause.

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var skip = function () {};
+
 var
   test = require('tape'),
   list = require('../index.js');
@@ -173,6 +175,85 @@ test('.where() single clause', function (assert) {
       }
     ];
 
+
+  assert.deepEqual(result, expected,
+    'should contain all the expected records.');
+
+  assert.equal(result.length, 2,
+    'should not contain excluded records.');
+
+  assert.deepEqual(records, copy,
+    'should not alter original list.');
+
+  assert.end();
+});
+
+
+skip('.where() multiple clause', function (assert) {
+  // Find the order where Dennis ordered a cymbal.
+  var records = [
+      {
+        "id": "ci6r6aliv00007poxc2zgnjvf",
+        "date": "2014-12-30 05:29:28",
+        "billingEmail": "dennis@example.com",
+        "firstName": "Dennis",
+        "lastName": "Chambers",
+        "lineItems": {
+          "name": "Zildjian K Custom Organic Ride - 21\"",
+          "sku": "h617xrh",
+          "quantity": "1",
+          "total": "379.95"
+        }
+      },
+      {
+        "id": "ci6r6aliv00007poxc2zgnjvf",
+        "date": "2014-12-30 05:29:28",
+        "billingEmail": "carlos@example.com",
+        "firstName": "Carlos",
+        "lastName": "Santana",
+        "lineItems": {
+          "name": "Gibson Memphis 1963 ES-335 TD - '60s Cherry 2014",
+          "sku": "sc37x3m",
+          "quantity": "1",
+          "total": "3999.00"
+        }
+      },
+      {
+        "id": "ci6r6aliv00007poxc2zgnjvf",
+        "date": "2014-12-30 05:29:28",
+        "billingEmail": "dennis@example.com",
+        "firstName": "Dennis",
+        "lastName": "Chambers",
+        "lineItems": {
+          "name": "DW Collector's Series Metal Snare - 6.5\"x14\" Titanium 1mm",
+          "sku": "sc37x3m",
+          "quantity": "1",
+          "total": "379.95"
+        }
+      }
+    ],
+    copy = slice.call(records),
+
+    result = list(records).where({
+      billingEmail: 'dennis@example.com',
+      sku: 'h617xrh'
+    }),
+
+    expected = [
+      {
+        "id": "ci6r6aliv00007poxc2zgnjvf",
+        "date": "2014-12-30 05:29:28",
+        "billingEmail": "dennis@example.com",
+        "firstName": "Dennis",
+        "lastName": "Chambers",
+        "lineItems": {
+          "name": "Zildjian K Custom Organic Ride - 21\"",
+          "sku": "h617xrh",
+          "quantity": "1",
+          "total": "379.95"
+        }
+      }
+    ];
 
   assert.deepEqual(result, expected,
     'should contain all the expected records.');
